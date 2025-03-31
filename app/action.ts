43bb72fbdaea78cf69d212e4d924d98c383b9659
Tapovan2-attendance <tapovan2attendance@gmail.com>
2025-03-31 10:41:56 +0530

    Update action.ts

--- app/action.ts
+++ app/action.ts
@@ -4,7 +4,7 @@ export const getStudent = async (standard: string, classId: string) => {
      console.log(standard, classId);
    
      const data = await fetch(
-       `https://tapovanmarks.vercel.app/api/students?standard=${standard}&class=${classId}`
+       `https://talod-tapovan.vercel.app/api/students?standard=${standard}&class=${classId}`
      );
      const datas = await data.json();
    
